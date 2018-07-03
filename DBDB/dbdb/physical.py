a2b9b8b1264ab4ab2aa6d193dcc0542a13f3a8a5
kewei <2409764492@qq.com>
2018-07-03 09:58:04 +0800

    物理层实现

---
+++ DBDB/dbdb/physical.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+import os
+import struct
+import portalocker
+
+
+class Storage(object):
+    SUPERBLOCK_SIZE = 4096
+    # 二进制转码参数
+    INTEGER_FORMAT = "!Q"
+    INTEGER_LENGTH = 8
+
+    def __init__(self, f):
+        self._f = f
+        self.locked = False
+        self._ensure_superblock()
+
+    def _ensure_superblock(self):
+        self.lock()
+        self._seek_end()
+        end_address = self._f.tell()
+        if end_address < self.SUPERBLOCK_SIZE:
+            self._f.write(b'\x00' * (self.SUPERBLOCK_SIZE - end_address))
+        return self.unlock()
+
+    def lock(self):
+        if not self.locked:
+            portalocker.lock(self._f, portalocker.LOCK_EX)
+            self.locked= True
+            return True
+        else:
+            return False
+
+    def unlock(self):
+        if self.locked:
+            self._f.flush()
+            portalocker.unlock(self._f)
+            self.locked = False
+
+    def _seek_end(self):
+        self._f.seek(0, os.SEEK_END)
+
+    def _seek_superblock(self):
+        self._f.seek(0)
+
+    def _bytes_to_integer(self, integer_bytes):
+        return struct.pack(self.INTEGER_FORMAT, integer_bytes)[0]
+
+    def _integer_to_bytes(self, integer):
+        return struct.unpack(self.INTEGER_FORMAT, integer)
+
+    def _read_integer(self):
+        return self._bytes_to_integer(self._f.read(self.INTEGER_LENGTH))
+
+    def _write_integer(self):
+        return self._bytes_to_integer(self._f.read(self.INTEGER_LENGTH))
